2636158736830e6e252092cdbd211467860113b4
hedilarbi <hedi.larbi@esprit.tn>
2025-07-22 13:30:46 +0100

    fixxx

--- src/app/(protected)/tableau-de-bord/commandes/[code]/page.js
+++ src/app/(protected)/tableau-de-bord/commandes/[code]/page.js
@@ -1,20 +1,19 @@
-// app/…/[code]/page.tsx
-
 import OrderComponent from "@/components/OrderComponent";
 import Link from "next/link";
+import React from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 
-export default async function Page({ params }: { params: { code: string } }) {
-  const { code } = params;
+const page = async ({ params }) => {
+  const { code } = await params;
   return (
     <div className="h-screen p-6 bg-gray-100 relative">
       <div className="max-w-7xl mx-auto">
-        <div className="flex items-center mb-6 space-x-4">
+        <div className="flex  items-center mb-6 space-x-4">
           <Link
             href="/tableau-de-bord"
             className="bg-blue-600 text-white rounded-full p-3 flex justify-center items-center"
           >
-            <FaArrowLeftLong />
+            <FaArrowLeftLong className="" />
           </Link>
           <h1 className="text-3xl font-bold text-gray-800">#{code}</h1>
         </div>
@@ -22,4 +21,6 @@ export default async function Page({ params }: { params: { code: string } }) {
       <OrderComponent code={code} />
     </div>
   );
-}
+};
+
+export default page;
